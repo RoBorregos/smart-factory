dcf95130b4b24279058e86790b1ee164a6da63be
aurotb <auro.tj@gmail.com>
2021-01-17 13:16:49 -0600

    Added Dashgo's ws

---
+++ dashgo_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/aurotb/Desktop/dashgo_ws/src
+# Build directory: /home/aurotb/Desktop/dashgo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(dashgo/dashgo_bringup)
+subdirs(dashgo/dashgo_description)
+subdirs(dashgo/dashgo_nav)
---
+++ dashgo_ws/build/dashgo/dashgo_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aurotb/Desktop/dashgo_ws/src/dashgo/dashgo_bringup
+# Build directory: /home/aurotb/Desktop/dashgo_ws/build/dashgo/dashgo_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
