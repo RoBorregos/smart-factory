9cb237306204f21d251acc9a74bb0573b8d16d19
aurotb <auro.tj@gmail.com>
2021-04-05 16:56:02 -0500

    Built succesfully

--- dashgo_ws/src/task_allocation/src/task_allocation_node.cpp
+++ dashgo_ws/src/task_allocation/src/task_allocation_node.cpp
@@ -5,36 +5,40 @@
 class TaskAllocator
 {
 
+public:
+    TaskAllocator(int argc, char **argv);
+    void publish_new_task(task_allocation::FactoryTask new_task);
+
 private:
     // ROS
     ros::NodeHandle* node_handle;
     ros::Publisher* task_auction_pub;
-    ros::Suscriber* task_bids_sus;
+    ros::Subscriber* task_bids_sus;
 
     // Bidder information
     int top_bidder;
     int top_bidder_cost;
 
     // Constants
-    const static string NODE_NAME = "TaskAllocator";
-    const static string PUBLISHER_TOPIC_NAME = "TaskAuction";
+    std::string NODE_NAME;
+    const  std::string PUBLISHER_TOPIC_NAME = "TaskAuction";
 };
 
-TaskAllocator::TaskAllocator()
+TaskAllocator::TaskAllocator(int argc, char **argv)
 {
-    ros::init(argc, argv, NODE_NAME);
-    node_handle = new ros::NodeHandle;
-    task_auction_pub = new node_handle.advertise<central::FactoryTask>(PUBLISHER_TOPIC_NAME, 1000);
+    
+    node_handle = new ros::NodeHandle();
+    // *task_auction_pub =  node_handle->advertise<task_allocation::FactoryTask>(PUBLISHER_TOPIC_NAME, 1000);
 }
 
-TaskAllocator::publish_new_task(central::FactoryTask new_task){
+void TaskAllocator::publish_new_task(task_allocation::FactoryTask new_task){
 
 }
-
-
 
 int main(int argc, char **argv)
 {
-    TaskAllocator TaskAllocator();
+    std::string NODE_NAME = "TaskAllocator";
+    ros::init(argc, argv, NODE_NAME);
+    TaskAllocator TaskAllocator(argc,argv);
     return 0;
 }
