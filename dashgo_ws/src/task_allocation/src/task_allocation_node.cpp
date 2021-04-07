c23f05f3d03f9420e58c4c5d77679d2e4e494728
aurotb <auro.tj@gmail.com>
2021-04-07 13:58:23 -0500

    Started input of tasks in console

--- dashgo_ws/src/task_allocation/src/task_allocation_node.cpp
+++ dashgo_ws/src/task_allocation/src/task_allocation_node.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include <ros/console.h>
 #include "task_allocation/FactoryTask.h"
 #include <string>
 
@@ -7,38 +8,66 @@ class TaskAllocator
 
 public:
     TaskAllocator(int argc, char **argv);
+    ~TaskAllocator();
     void publish_new_task(task_allocation::FactoryTask new_task);
+    void console_input_tasks();
 
 private:
     // ROS
-    ros::NodeHandle* node_handle;
-    ros::Publisher* task_auction_pub;
-    ros::Subscriber* task_bids_sus;
+    ros::NodeHandle *node_handle;
+    ros::Publisher task_auction_pub;
+    ros::Subscriber task_bids_sus;
 
     // Bidder information
     int top_bidder;
     int top_bidder_cost;
 
     // Constants
-    std::string NODE_NAME;
-    const  std::string PUBLISHER_TOPIC_NAME = "TaskAuction";
+    static std::string NODE_NAME;
+    static std::string PUBLISHER_TOPIC_NAME;
 };
 
 TaskAllocator::TaskAllocator(int argc, char **argv)
 {
-    
+
+    ros::init(argc, argv, NODE_NAME);
     node_handle = new ros::NodeHandle();
-    // *task_auction_pub =  node_handle->advertise<task_allocation::FactoryTask>(PUBLISHER_TOPIC_NAME, 1000);
+    task_auction_pub = node_handle->advertise<task_allocation::FactoryTask>(PUBLISHER_TOPIC_NAME, 1000);
+    ROS_DEBUG("Initialized TaskAllocator");
 }
 
-void TaskAllocator::publish_new_task(task_allocation::FactoryTask new_task){
-
+TaskAllocator::~TaskAllocator()
+{
+    delete node_handle;
 }
+
+void TaskAllocator::console_input_tasks()
+{
+    int userInputActionType;
+
+    while (ros::ok)
+    {
+        // Ask for user input of new task and pusblish it
+        task_allocation::FactoryTask new_factory_task;
+        std::cout << "Please input the actionType of the task (number): ";
+        std::cin >> userInputActionType;
+    }
+}
+
+void TaskAllocator::publish_new_task(task_allocation::FactoryTask new_task)
+{
+}
+
+std::string TaskAllocator::PUBLISHER_TOPIC_NAME = "TaskAuction";
+std::string TaskAllocator::NODE_NAME = "TaskAllocatorNode";
 
 int main(int argc, char **argv)
 {
-    std::string NODE_NAME = "TaskAllocator";
-    ros::init(argc, argv, NODE_NAME);
-    TaskAllocator TaskAllocator(argc,argv);
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+    {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
+    TaskAllocator TaskAllocator(argc, argv);
     return 0;
 }
