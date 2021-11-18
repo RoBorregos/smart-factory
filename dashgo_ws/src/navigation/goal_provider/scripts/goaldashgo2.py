6023f8a63279f3131602d90a634c336488cc9b18
EdisonAltamirano <a00825234@itesm.mx>
2021-11-18 04:02:34 +0000

    testplc

--- dashgo_ws/src/navigation/goal_provider/scripts/goaldashgo2.py
+++ dashgo_ws/src/navigation/goal_provider/scripts/goaldashgo2.py
@@ -103,12 +103,14 @@ class StateMachine:
             newcoordinatez = plcregisters[6]
             if(self.oldcoordinatex !=newcoordinatex or self.oldcoordinatey !=newcoordinatey or  self.oldcoordinatez !=newcoordinatez ):
                 self.send_infomodbus(9,1) #Update Status occupied
-            self.sendGoal1(a)
-            rospy.loginfo("Llego misión en: ")
-            if(self.oldcoordinatex !=newcoordinatex or self.oldcoordinatey !=newcoordinatey or self.oldcoordinatez !=newcoordinatez ):
-                self.send_infomodbus(9,2) #Update Status success
-                rospy.sleep(5)
-                self.send_infomodbus(9,0) #Update Status success
+            status = self.sendGoal1(a)
+            rospy.loginfo("Llego mision en ")
+            rospy.loginfo(status)
+            if(status):
+                if(self.oldcoordinatex !=newcoordinatex or self.oldcoordinatey !=newcoordinatey or self.oldcoordinatez !=newcoordinatez ):
+                    self.send_infomodbus(9,2) #Update Status success
+                    rospy.sleep(5)
+                    self.send_infomodbus(9,0) #Update Status success
             socket_pub.publish("arrive")
             self.oldcoordinatex = newcoordinatex
             self.oldcoordinatey = newcoordinatey
